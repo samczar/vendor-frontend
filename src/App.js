29cdf62d18d30d81cd1c27756001a1bd4cd21c5d
Chibuzo Anyanka <chibuzoanyanka@Chibuzos-MBP.fritz.box>
2020-08-15 20:58:52 +1200

    redux config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.css'
+import { Router, Switch, Route } from 'react-router-dom'
+import { Login } from './_components/login'
+import { Home } from './_components/home'
+import { history } from './_helpers'
+import PrivateRoute from './_components/PrivateRoute'
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Router history={history}>
+					<div>
+						<Switch>
+							<PrivateRoute exact path="/home" component={Home} />
+							<Route exact path="/" component={Login} />
+						</Switch>
+					</div>
+				</Router>
+			</div>
+		)
+	}
 }
-
-export default App;
+export default App
